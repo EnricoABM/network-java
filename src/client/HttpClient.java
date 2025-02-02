215bc9260c7c12fd8f0a8f830500b253eb83f76a
Enrico Bertozzi <enricodeabm@hotmail.com>
2025-02-01 21:44:34 -0300

    Http Cliente done

--- src/client/HttpClient.java
+++ src/client/HttpClient.java
@@ -1,35 +1,81 @@
 package client;
 
+import model.RequestHttp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Map;
+import java.util.Set;
 
 public class HttpClient {
+
     public static void main(String[] args) {
         try {
-            Socket cliente = new Socket("127.0.0.1", 8080);
+            RequestHttp request = new RequestHttp();
+            request.setMethod("GET");
+            request.setUri("/ws/01001000/json/");
+            request.setHttpVersion("HTTP/1.1");
+            request.addHeader("Accept", "application/json");
+            request.addHeader("Host", "viacep.com.br");
 
-            PrintStream ps = new PrintStream(cliente.getOutputStream());
-
-            ps.print("GET /nome/Enrico HTTP/1.1\r\n");
-            ps.print("Host: localhost\r\n");
-            ps.print("Connection: close\r\n");  // Indica ao servidor que pode fechar a conexão
-            ps.print("\r\n");  // Fim dos cabeçalhos
-            ps.flush();  // Garante que tudo foi enviado
-
-            BufferedReader response = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-
-            String linha;
-            while ((linha = response.readLine()) != null) {
-                System.out.println(linha);
-            }
-
-            cliente.close();
+            HttpClient client = new HttpClient();
+            String response = client.send(request, "viacep.com.br", 80);
+            System.out.println(response);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+
         }
 
+    }
+
+    public String send(RequestHttp request, String address, Integer port) throws IOException {
+        Socket client = new Socket(address, port);
+        PrintStream ps = new PrintStream(client.getOutputStream());
+
+        String requestBody = this.createRequestBody(request);
+
+        ps.print(requestBody);
+        ps.flush();
+
+        String response = this.readResponse(client);
+        client.close();
+        return response;
+    }
+
+    private String readResponse(Socket client) throws IOException {
+        BufferedReader response = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        StringBuilder responseString = new StringBuilder();
+
+        String line;
+        while((line = response.readLine()) != null) {
+            responseString.append(line);
+            responseString.append("\n");
+        }
+
+        return responseString.toString();
+    }
+
+    private String createRequestBody(RequestHttp request) {
+        StringBuilder requestString = new StringBuilder();
+        requestString
+                .append(request.getMethod())
+                .append(" ")
+                .append(request.getUri())
+                .append(" ")
+                .append(request.getHttpVersion())
+                .append("\r\n");
+
+        Set<Map.Entry<String, String>> headers = request.getHeaders().entrySet();
+        for (Map.Entry<String, String> header: headers) {
+            requestString
+                    .append(header.getKey())
+                    .append(": ")
+                    .append(header.getValue())
+                    .append("\r\n");
+        }
+        requestString.append("\r\n");
+        return requestString.toString();
     }
 }
